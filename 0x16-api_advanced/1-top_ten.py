d8ddb34822a9f2e2e582a111ef81589cc87718dd
yves3a <yves.irakoz3@gmail.com>
2025-03-03 20:31:46 +0200

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
+
+"""This script queries the Reddit API and prints the titles of the first 10 top
+posts for a give subreddit."""
 
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+def top_ten(subreddit):
+    """
+    Prints the titles of the top 10 hot posts from a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    # Send a GET request to the Reddit API to retrieve the top 10 hot posts
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "nabuntu_bot-01"},
+        allow_redirects=False,
+    )
+
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(None)
+        return
+
+    # Print the titles of the top 10 hot posts
+    for post in response.json()["data"]["children"]:
+        print(post["data"]["title"])
